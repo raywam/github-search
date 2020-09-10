61f10be4bd9f31c046d951e67cbd1d0750f4e122
Raywam <raywam@hotmail.com>
2020-09-10 08:30:19 -0300

    [BUG] search url redirect

--- src/components/InputSearch/index.js
+++ src/components/InputSearch/index.js
@@ -3,11 +3,12 @@ import './styles.scss';
 import React from 'react';
 
 import SearchIcon from '../../assets/icons/Search-Icon.svg';
+import { Redirect, Link } from 'react-router-dom';
 
 class InputSearch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: null };
+    this.state = { value: null, redirect: false };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -19,13 +20,25 @@ class InputSearch extends React.Component {
   }
 
   handleSubmit(event) {
-    this.props.sendData(this.state.value);
+    if (this.props.sendData) {
+      this.props.sendData(this.state.value);
+    }
+
+    this.setState({ redirect: true });
     event.preventDefault();
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      this.setState({ redirect: false });
+      return <Redirect to={{ pathname: '/results', search: `?user=${this.state.value}` }} />
+    }
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
+        {this.renderRedirect()}
         <div className="Search-Input-Component">
           <input className="Search-Input" type="text" maxLength="50" value={this.state.value !== null ? this.state.value : this.props.username} onChange={this.handleChange.bind(this)}></input>
           <button>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,16 @@
 import './styles.scss';
 
 import React from 'react';
-import { Redirect } from 'react-router-dom'
 import InputSearch from '../../components/InputSearch';
 import LogoApp from '../../components/LogoApp';
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { redirect: false, username: '' };
-
-    this.getUser = this.getUser.bind(this);
-  }
-
-  getUser(username) {
-    this.setState({
-      redirect: true,
-      username: username
-    });
-  }
-
-  renderRedirect = () => {
-    if (this.state.redirect) {
-      return <Redirect to={{ pathname: '/results', search: `?user=${this.state.username}` }} />
-    }
-  }
-
   render() {
     return (
       <div className="Home">
         <div>
           <LogoApp></LogoApp>
-          {this.renderRedirect()}
-          <InputSearch sendData={this.getUser}></InputSearch>
+          <InputSearch></InputSearch>
         </div>
       </div>
     )
